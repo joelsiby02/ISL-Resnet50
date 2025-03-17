53ff00a5097398c00cbf444fba98adc7dfea2ca1
Joel Siby <126755343+joelsiby02@users.noreply.github.com>
2025-03-18 01:57:08 +0530

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -22,8 +22,8 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 os.makedirs(RESULT_FOLDER, exist_ok=True)
 
 # Load YOLO model
-MODEL_PATH = "my_model.pt"
-model = YOLO(MODEL_PATH)
+model = YOLO("my_model.pt")
+model.export(format="torchscript", device='cpu')  # Creates my_model.torchscript
 
 def convert_video_to_mp4(input_path, output_path):
     cap = cv2.VideoCapture(input_path)
